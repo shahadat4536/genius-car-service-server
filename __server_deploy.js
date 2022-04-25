964e2e63e713e44d3daa6f24de5705929eb3bed5
shahadat4536 <shahadathossain4536@gmail.com>
2022-04-25 23:05:04 +0600

    last

--- __server_deploy.js
+++ __server_deploy.js
@@ -1,7 +1,18 @@
 /**
- * ONE TIME
+ * ---------------------------
+ * ONE TIME FOR YOUR COMPUTER
+ * ---------------------------
  * 1. create heroku account
  * 2. verify email
  * 3, install heroku cli
  * 4. heroku login
+ * ------------------------------
+ * Fir each project one time
+ * -----------------------------
+ * 1. heroku create
+ * 2. make sure you: git add . git commit. git push
+ * 3. 3. git push heroku main
+ * 4. go to Heroku dashboard > Current Project > Setting > Reveal Congig
+ * 5. copy paste config vars from your .env file
+ * 6. Make sure you have whitelisted all ip access mongodb
  */
--- index.js
+++ index.js
@@ -112,4 +112,8 @@ app.get("/", (req, res) => {
   res.send("Running Genius Server");
 });
 
+app.get("/hero", (req, res) => {
+  res.send("Hero meets hero ku");
+});
+
 app.listen(port, () => console.log("object", port));
